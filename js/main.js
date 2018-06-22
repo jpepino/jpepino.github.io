cd2ee5efeca13bf660f6adb64b8b3a55fb6240cc
Jeremy Pepino <jpepino@gatech.edu>
2018-06-22 18:29:56 -0400

    Updated page routing

--- js/main.js
+++ js/main.js
@@ -14,13 +14,11 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/game", {templateUrl: "partials/game.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    .when("/tutorial", {templateUrl: "partials/tutorial.html", controller: "BlogCtrl"})
+    .when("/tutorial_item", {templateUrl: "partials/tutorial_item.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
