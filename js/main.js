afb1bee134d784f4bb0101e7454401cad45043b1
Jeremy Pepino <jpepino@gatech.edu>
2018-07-07 21:53:19 -0400

    navigation

--- js/main.js
+++ js/main.js
@@ -16,9 +16,15 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/game", {templateUrl: "partials/game.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
+    // Tutorial
     .when("/tutorial", {templateUrl: "partials/tutorial.html", controller: "BlogCtrl"})
     .when("/tutorial_item", {templateUrl: "partials/tutorial_item.html", controller: "BlogCtrl"})
+    .when("/tutorial1-1", {templateUrl: "partials/tutorial1-1.html", controller: "BlogCtrl"})
+    .when("/tutorial1-2", {templateUrl: "partials/tutorial1-2.html", controller: "BlogCtrl"})
+    .when("/tutorial1-3", {templateUrl: "partials/tutorial1-3.html", controller: "BlogCtrl"})
+    .when("/tutorial1-4", {templateUrl: "partials/tutorial1-4.html", controller: "BlogCtrl"})
+    .when("/tutorial1-5", {templateUrl: "partials/tutorial1-5.html", controller: "BlogCtrl"})
+    .when("/tutorial1-6", {templateUrl: "partials/tutorial1-6.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
